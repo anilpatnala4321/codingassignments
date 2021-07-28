d6fbeb65c9edb17b83a289220d52d243a8e16440
anilpatnala4321 <88071280+anilpatnala4321@users.noreply.github.com>
2021-07-28 09:25:20 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+
+import java.applet.Applet;
+import java.awt.Graphics;
+public class First extends Applet
+{
+public void paint(Graphics g)    
+{
+g.drawString("welcome",200,200);
+}
+}
+/* <applet code="First.class" height="200" width="400">
+</applet>*/
